d7bf0792e5de79e7b05bbeca079f0989546000a1
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2018-12-19 14:41:55 +0100

    new one

--- Swype/PI/write.py
+++ Swype/PI/write.py
@@ -1,1 +1,19 @@
-#need to be implemented
+import serial
+import time
+
+ser = serial.Serial(
+    port='/dev/ttyAMA0',
+    baudrate= 115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+time.sleep(1)
+
+def write_Shortcut(shortcutNumber):
+    ser.write(('SH' +shortcutNumber))
+
+def write_Movement(x,y):
+    ser.write(x + ',' + y)
+
