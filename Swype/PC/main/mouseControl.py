594a262f3db959bff627412716486df375bd17ea
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2018-12-25 13:41:07 +0100

    stuff

--- Swype/PC/main/mouseControl.py
+++ Swype/PC/main/mouseControl.py
@@ -1,8 +1,11 @@
 import pyautogui
 
 def handleShortcut(command, shortcut):
-    pyautogui.hotkey(shortcut.split(","))
+    switcher = {
+        'hotkey': pyautogui.hotkey(shortcut.split)
+    }
 
 def handleMouse(xMovement,yMovement,speed):
     x,y = pyautogui.position()
     pyautogui.moveTo(x + int(xMovement), y + int(yMovement), speed, pyautogui.easeOutQuad)
+
