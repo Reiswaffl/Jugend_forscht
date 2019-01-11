edbdbcc3472815c3f6b94eecc8aeca1db380ae3e
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2019-01-11 23:12:03 +0100

    GUI

--- Swype/PC/main/userInterface.py
+++ Swype/PC/main/userInterface.py
@@ -18,9 +18,17 @@ class userInterface(tkr.Tk):
             tkr.StringVar(),
             tkr.StringVar(),
             tkr.StringVar(),
+            tkr.StringVar(),
+            tkr.StringVar(),
+            tkr.StringVar(),
+            tkr.StringVar(),
             tkr.StringVar()
         ]
         self.cmd = [
+            tkr.StringVar(),
+            tkr.StringVar(),
+            tkr.StringVar(),
+            tkr.StringVar(),
             tkr.StringVar(),
             tkr.StringVar(),
             tkr.StringVar(),
@@ -50,12 +58,18 @@ class userInterface(tkr.Tk):
         self.sh[1].set(self.SHORTCUTS[1])
         self.sh[2].set(self.SHORTCUTS[2])
         self.sh[3].set(self.SHORTCUTS[3])
-
+        self.sh[4].set(self.SHORTCUTS[0])
+        self.sh[5].set(self.SHORTCUTS[1])
+        self.sh[6].set(self.SHORTCUTS[2])
+        self.sh[7].set(self.SHORTCUTS[3])
         self.cmd[0].set(self.COMMAND[0])
         self.cmd[1].set(self.COMMAND[0])
         self.cmd[2].set(self.COMMAND[0])
         self.cmd[3].set(self.COMMAND[0])
-
+        self.cmd[4].set(self.COMMAND[0])
+        self.cmd[5].set(self.COMMAND[0])
+        self.cmd[6].set(self.COMMAND[0])
+        self.cmd[7].set(self.COMMAND[0])
     def select(self):
         print("selected " + self.sh.get() + " for Shortcut 1")
         print("selected " + self.sh2.get() + " for Shortcut 2")
@@ -69,10 +83,10 @@ class userInterface(tkr.Tk):
     def buildInterface(self):
         self.geometry("900x600")
         self.title("Swypad Window")
-
+        self.buildCommandsStack(0,1,1)
         self.buildShortcutsStack(0,1,2)
+        self.buildCommandsStack(4,1,4)
         self.buildShortcutsStack(4,1,5)
-        self.buildCommandsStack(0,1,2)
         self.buildLabels()
 
         selectbutton = tkr.Button(master=self, text="Select", command=self.select, font=("Arial", 16))
@@ -80,8 +94,8 @@ class userInterface(tkr.Tk):
 
     def buildShortcutsStack(self,index, rowOffset, columnOffset):
         for i in range(4):
-            print(str(i) + "   " + str(i+index))
-            self.shortcut[i+index] = tkr.OptionMenu(self,self.sh[i%4],*self.SHORTCUTS)
+            print(str(i+index) + "   " + str(columnOffset) + "   " + str(rowOffset+(i%4)))
+            self.shortcut[i+index] = tkr.OptionMenu(self,self.sh[i],*self.SHORTCUTS)
             self.shortcut[i+index].configure(font=("Arial", 16))
             self.shortcut[i+index].grid(row=(rowOffset+(i%4)),column=columnOffset)
 
@@ -90,15 +104,16 @@ class userInterface(tkr.Tk):
         for i in range(4):
             self.command[i+index] = tkr.OptionMenu(self,self.cmd[i],*self.COMMAND)
             self.command[i+index].configure(font=("Arial",16))
-            self.command[i+index].grid(row=(rowOffset+i),column=columnOffset)
+            self.command[i+index].grid(row=(rowOffset+(i%4)),column=columnOffset)
 
     def buildLabels(self):
         tkr.Label(self, text="Shortcutoptions", font=("Arial", 18)).grid(row=0)
 
         tkr.Label(self, text="Shortcut 1", font=("Arial", 16)).grid(row=1, column=0)
-
         tkr.Label(self, text="Shortcut 2", font=("Arial", 16)).grid(row=2, column=0)
-
         tkr.Label(self, text="Shortcut 3", font=("Arial", 16)).grid(row=3, column=0)
-
         tkr.Label(self, text="Shortcut 4", font=("Arial", 16)).grid(row=4, column=0)
+        tkr.Label(self, text="Shortcut 5", font=("Arial", 16)).grid(row=1, column=3)
+        tkr.Label(self, text="Shortcut 6", font=("Arial", 16)).grid(row=2, column=3)
+        tkr.Label(self, text="Shortcut 7", font=("Arial", 16)).grid(row=3, column=3)
+        tkr.Label(self, text="Shortcut 8", font=("Arial", 16)).grid(row=4, column=3)
