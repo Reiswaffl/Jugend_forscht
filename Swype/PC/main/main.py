22467f183e4bcc67f5830304f43e1f63a0da58bf
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2019-01-14 12:44:56 +0100

    GUI backend

--- Swype/PC/main/main.py
+++ Swype/PC/main/main.py
@@ -1,8 +1,7 @@
 import userInterface
 import subprocess
 
-p = subprocess.Popen(['python','serialHandling.py','arg1','arg2'])
 interface = userInterface.userInterface()
 interface.buildInterface()
 interface.mainloop()
-p.terminate()
+
--- Swype/PC/main/userInterface.py
+++ Swype/PC/main/userInterface.py
@@ -1,7 +1,9 @@
 import Tkinter as tkr
 import logic
+import subprocess
 
 class userInterface(tkr.Tk):
+    serialRunning = False
     def __init__(self):
         tkr.Tk.__init__(self)
         self.SHORTCUTS = [
@@ -71,26 +73,35 @@ class userInterface(tkr.Tk):
         self.cmd[6].set(self.COMMAND[0])
         self.cmd[7].set(self.COMMAND[0])
     def select(self):
-        logic.writeShortcut('01',self.cmd[0].get(),  self.sh[0].get())
-        logic.writeShortcut('02',self.cmd[1].get(), self.sh[1].get())
-        logic.writeShortcut('03',self.cmd[2].get(), self.sh[2].get())
-        logic.writeShortcut('04',self.cmd[3].get(), self.sh[3].get())
-        logic.writeShortcut('05',self.cmd[4].get(),  self.sh[4].get())
-        logic.writeShortcut('06',self.cmd[5].get(), self.sh[5].get())
-        logic.writeShortcut('07',self.cmd[6].get(), self.sh[6].get())
-        logic.writeShortcut('08',self.cmd[7].get(), self.sh[7].get())
+        logic.writeShortcut('0',self.cmd[0].get(),  self.sh[0].get())
+        logic.writeShortcut('1',self.cmd[1].get(), self.sh[1].get())
+        logic.writeShortcut('2',self.cmd[2].get(), self.sh[2].get())
+        logic.writeShortcut('3',self.cmd[3].get(), self.sh[3].get())
+        logic.writeShortcut('4',self.cmd[4].get(),  self.sh[4].get())
+        logic.writeShortcut('5',self.cmd[5].get(), self.sh[5].get())
+        logic.writeShortcut('6',self.cmd[6].get(), self.sh[6].get())
+        logic.writeShortcut('7',self.cmd[7].get(), self.sh[7].get())
+
+    def start(self):
+        if self.serialRunning == False:
+            self.p = subprocess.Popen(['python', 'serialHandling.py', 'arg1', 'arg2'])
+            self.startButton.config(text="stop Connection")
+            self.serialRunning = True
+        else:
+            self.p.terminate()
+            self.startButton.config(text="start Connection")
+            self.serialRunning = False
 
     def buildInterface(self):
-        self.geometry("1500x600")
+        self.geometry("1560x600")
         self.title("Swypad Window")
+        self.loadInformation()
         self.buildCommandsStack(0,1,1)
         self.buildShortcutsStack(0,1,2)
         self.buildCommandsStack(4,1,4)
         self.buildShortcutsStack(4,1,5)
+        self.buildRest()
         self.buildLabels()
-
-        selectbutton = tkr.Button(master=self, text="Select", command=self.select, font=("Arial", 16))
-        selectbutton.grid(row=5, column=0)
 
     def buildShortcutsStack(self,index, rowOffset, columnOffset):
         for i in range(4):
@@ -106,9 +117,20 @@ class userInterface(tkr.Tk):
             self.command[i+index].configure(font=("Arial",16))
             self.command[i+index].grid(row=(rowOffset+(i%4)),column=columnOffset)
 
+    def buildRest(self):
+        self.selectbutton = tkr.Button(master=self, text="Select", command=self.select, font=("Arial", 16))
+        self.selectbutton.grid(row=5, column=0)
+
+        self.comBox = tkr.Entry()
+        self.comBox.grid(row=0, column=2)
+        self.comBox.insert(0,"COM1")
+
+        self.startButton = tkr.Button(master=self,text="start Connection",command=self.start,font=("Arial",14))
+        self.startButton.grid(row=0, column=3)
+
     def buildLabels(self):
         tkr.Label(self, text="Shortcutoptions", font=("Arial", 18)).grid(row=0)
-
+        tkr.Label(self, text="COM: ",font=("Arial",16)).grid(row=0, column=1)
         tkr.Label(self, text="Shortcut 1", font=("Arial", 16)).grid(row=1, column=0)
         tkr.Label(self, text="Shortcut 2", font=("Arial", 16)).grid(row=2, column=0)
         tkr.Label(self, text="Shortcut 3", font=("Arial", 16)).grid(row=3, column=0)
@@ -117,6 +139,8 @@ class userInterface(tkr.Tk):
         tkr.Label(self, text="Shortcut 6", font=("Arial", 16)).grid(row=2, column=3)
         tkr.Label(self, text="Shortcut 7", font=("Arial", 16)).grid(row=3, column=3)
         tkr.Label(self, text="Shortcut 8", font=("Arial", 16)).grid(row=4, column=3)
-    def load(self):
+    def loadInformation(self):
         for i in range(8):
-            self.sh[i].set(logic.)
+            c,s = logic.getShortCut(str(i))
+            self.sh[i].set(s)
+            self.cmd[i].set(c)
