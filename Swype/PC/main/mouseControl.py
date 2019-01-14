71f3e2706e034aa36bdf485ff1cc985968339b25
Direcx <mel.langfeld@gmx.de>
2019-01-14 11:56:35 +0100

    reformated

--- Swype/PC/main/mouseControl.py
+++ Swype/PC/main/mouseControl.py
@@ -1,25 +1,28 @@
 import pyautogui
 
+
 def handleShortcut(command, shortcut):
     if command == 'hotkey':
         hotkey(shortcut)
     if command == 'scroll':
         scroll(shortcut)
 
-def handleMouse(xMovement,yMovement,speed):
-    x,y = pyautogui.position()
+
+def handleMouse(xMovement, yMovement, speed):
+    x, y = pyautogui.position()
     pyautogui.moveTo(x + int(xMovement), y + int(yMovement), speed, pyautogui.easeOutQuad)
 
 
 def hotkey(shortcut):
     sh = shortcut.split(',')
     if len(sh) == 2:
-        pyautogui.hotkey(sh[0],sh[1])
+        pyautogui.hotkey(sh[0], sh[1])
     if len(sh) == 3:
-        pyautogui.hotkey(sh[0],sh[1],sh[2])
+        pyautogui.hotkey(sh[0], sh[1], sh[2])
     if len(sh) == 4:
-        pyautogui.hotkey(sh[0],sh[1],sh[2],sh[3])
+        pyautogui.hotkey(sh[0], sh[1], sh[2], sh[3])
+
 
 def scroll(shortcut):
-    i = shortcut.replace('i','')
+    i = shortcut.replace('i', '')
     pyautogui.scroll(int(i))
