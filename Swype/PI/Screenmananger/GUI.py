7f74c16fa5df82b8255a61d0fb575723a2acc975
Direcx <mel.langfeld@gmx.de>
2019-01-14 22:01:26 +0100

    Pi Kivy added

---
+++ Swype/PI/Screenmananger/GUI.py
@@ -0,0 +1,89 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.config import Config
+from kivy.properties import ObjectProperty
+Config.set('graphics', 'width', '1024')
+Config.set('graphics', 'height', '600')
+
+touch = False
+lasttime = 0
+previousValue = 0
+currentValue = 0
+operator = "+"
+
+
+class MainScreen(Screen):
+    pass
+
+
+class Calculator(Screen):
+    text = ObjectProperty("0")
+
+    def changeoperator(self, noperator):
+        global operator
+        global currentValue
+        global previousValue
+        if previousValue == 0:
+            previousValue = currentValue
+        else:
+            self.totalup(currentValue)
+        operator = noperator
+        currentValue = 0
+
+    def giveresult(self):
+        global currentValue
+        global previousValue
+        self.totalup(currentValue)
+        previousValue = 0
+        currentValue = 0
+
+    def reset(self):
+        global previousValue
+        global currentValue
+        currentValue = 0
+        previousValue = 0
+        self.text = str(0)
+
+    def calculate(self, value):
+        global operator
+        global currentValue
+        currentValue *= 10
+        currentValue += value
+        self.text = str(currentValue)
+
+    def totalup(self, value2):
+        global operator
+        global previousValue
+        if operator == "+":
+            previousValue += value2
+            self.text = str(previousValue)
+        elif operator == "-":
+            previousValue -= value2
+            self.text = str(previousValue)
+        elif operator == "*":
+            previousValue *= value2
+            self.text = str(previousValue)
+        elif operator == "/":
+            if value2 != 0:
+                previousValue /= value2
+                self.text = str(previousValue)
+            else:
+                self.text = "Error"
+
+
+class ScreenManagement(ScreenManager):
+    pass
+
+
+presentation = Builder.load_file("main.kv")
+
+
+class MainApp(App):
+
+    def build(self):
+        return presentation
+
+
+if __name__ == "__main__":
+    MainApp().run()
--- Swype/PI/write.py
+++ Swype/PI/write.py
@@ -3,7 +3,7 @@ import time
 
 ser = serial.Serial(
     port='/dev/ttyAMA0',
-    baudrate= 115200,
+    baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
@@ -11,9 +11,10 @@ ser = serial.Serial(
 )
 time.sleep(1)
 
+
 def write_Shortcut(shortcutNumber):
-    ser.write(bin('SH' +shortcutNumber + '\n'))
+    ser.write(bin('SH' + shortcutNumber + '\n'))
 
-def write_Movement(x,y):
-    ser.write(bin(x + ',' + y +'\n'))
 
+def write_Movement(x, y):
+    ser.write(bin(x + ',' + y + '\n'))
