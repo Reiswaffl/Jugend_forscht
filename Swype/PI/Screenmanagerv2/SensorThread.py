afff58b6a21c5722d1c217d3939e6776f65ceeb7
Direcx <mel.langfeld@gmx.de>
2019-03-16 19:55:57 +0100

    Update

---
+++ Swype/PI/Screenmanagerv2/SensorThread.py
@@ -0,0 +1,23 @@
+import threading
+import time
+
+
+class Sensor(threading.Thread):
+    def __init__(self, sensor):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.sensor = sensor
+        print('sensor init done')
+
+    def run(self):
+        print("SensorThread initialized")
+        while True:
+            if self.sensor.receiveActivity():
+                self.sensor.callZpos()
+            self.sensor.handleDataStraight()
+            self.sensor.receiveGesture()
+            self.sensor.checkGesture()
+            self.sensor.handleDataStraight()
+            time.sleep(0.03)
+
+sensorThread = Sensor(sensor)
---
+++ Swype/PI/Screenmanagerv2/ZXsensorController.py
@@ -0,0 +1,195 @@
+# coding=utf-8
+import smbus
+import time
+
+
+class SensorController:
+    def __init__(self, writer):
+
+        self.gesture = 0
+        self.z_pos = 0
+        self.pointZero = 0
+        self.z_pos_old = 0
+        self.bus = smbus.SMBus(1)
+        self.state = 0
+        self.activity = False
+        self.start_time = time.clock()
+        self.tolerance = 10
+        self.t2 = 3
+        self.t3 = 10
+        self.writer = writer
+        self.lastActivity = False
+        print("Sensor init done")
+
+    def receiveActivity(self):
+        # global lastActivity
+        # global activity
+        status = self.bus.read_byte_data(0x10, 0x00)
+        if status == 129:
+            self.activity = True
+            self.lastActivity = True
+        elif status == 1:
+            self.activity = True
+            self.lastActivity = True
+        else:
+            if self.lastActivity == True:
+                self.lastActivity = False
+                self.activity = True
+            else:
+                self.activity = False
+        # print(self.activity)
+        return self.activity
+
+    def receiveZpos(self):
+        self.z_pos_old = self.z_pos
+        # self.z_pos = self.bus.read_byte_data(0x10, 0x0A)
+        return self.z_pos
+
+    def callZpos(self):
+        global z_pos
+        data = 0
+        notyero = 0
+        for x in range(0, 8):
+            datacall = self.bus.read_byte_data(0x10, 0x0A)
+            if not datacall == 0:
+                data += datacall
+                notyero += 1
+        if notyero >= 5:
+            pos = data / notyero
+        else:
+            pos = 0
+        self.z_pos_old = self.z_pos
+        self.z_pos = pos
+        return pos
+
+    def receiveGesture(self):
+        self.gesture = self.bus.read_byte_data(0x10, 0x04)
+
+    def checkGesture(self):
+        if self.gesture == 1:
+            print("swipe")
+        elif self.gesture == 2:
+            print("swipe 2")
+
+    def handleData(self):
+        print("_______wrong_____")
+        # print(self.z_pos)
+        if self.state == 0:
+            if self.receiveActivity():
+                self.start_time = time.clock()
+                self.state = 1
+            self.checkGesture()
+        elif self.state == 1:
+            # print("state 1")
+            dt = time.clock() - self.start_time
+            if self.receiveActivity():
+                if dt > 0.002:
+                    self.state = 2
+                    self.pointZero = self.z_pos
+                    print("||||" + str(self.z_pos) + "||||")
+            else:
+                self.state = 0
+            self.checkGesture()
+        elif self.state == 2:
+            print("state 2")
+            if not self.receiveActivity():
+                self.state = 0
+            elif self.z_pos == 0:
+                self.state = 0
+            elif self.z_pos > (self.pointZero + self.tolerance):
+                print("switch to state 3")
+                self.state = 3
+            elif self.z_pos < (self.pointZero - self.tolerance):
+                print("switch to state 5")
+                self.state = 5
+            else:
+                self.writer.write_soundchange('+')
+                self.writer.write_soundchange('-')
+                pass
+        elif self.state == 3:
+            print("state 3")
+            if not self.receiveActivity():
+                self.state = 0
+            elif self.z_pos > self.z_pos_old:
+                print("Lautstärke HOCH!")
+                self.writer.write_soundchange('+')
+            elif self.z_pos < (self.z_pos_old - (self.tolerance / 2)):
+                self.state = 4
+        elif self.state == 4:
+            if not self.receiveActivity():
+                self.state = 0
+            elif self.z_pos <= self.pointZero:
+                self.pointZero = self.z_pos
+                self.state = 2
+        elif self.state == 5:
+            print("state 5")
+            if not self.receiveActivity():
+                self.state = 0
+            elif self.z_pos == 0:
+                self.state = 0
+            elif self.z_pos < self.z_pos_old:
+                print("Lautstärke RUNTER!")
+                self.writer.write_soundchange('-')
+            elif self.z_pos >= (self.pointZero + (self.tolerance / 2)):
+                self.state = 2
+        elif self.state == 6:
+            if self.receiveActivity() == 0:
+                self.state = 0
+            elif self.z_pos >= self.pointZero:
+                self.pointZero = self.z_pos
+        elif self.state == 7:
+            pass
+        else:
+            self.state = 0
+
+    def handleDataStraight(self):
+        if self.state == 0:
+            if self.receiveActivity():
+                self.state = 1
+                self.start_time = time.clock()
+                self.z_pos_old = self.receiveZpos()
+        elif self.state == 1:
+            if self.receiveActivity():
+                dt = time.clock() - self.start_time
+                if dt > 0.01:
+                    self.state = 2
+                    # print("success")
+            else:
+                self.state = 0
+        elif self.state == 2:  # Einstellmodus, unterscheidet zwischen leichten Bewegungen und schnellen
+            if self.receiveActivity():
+                # print(str(self.z_pos) + "   o: " + str(self.z_old))
+                if self.z_pos > self.z_pos_old + self.t2 + self.t3:
+                    self.state = 3
+                    print("fast up")
+                elif self.z_pos < self.z_pos_old - self.t2 - self.t3:
+                    self.state = 4
+                    print("fast down")
+                elif self.z_pos > self.z_pos_old + self.t2:
+                    self.writer.write_soundchange('+')
+                    print("up")
+                    # print(self.z_pos)
+                elif self.z_pos < self.z_pos_old - self.t2:
+                    print("down")
+                    # print(self.z_pos)
+                    self.writer.write_soundchange('-')
+                else:
+                    # print("nothing")
+                    pass
+        elif self.state == 3:  # schnelle Hochbewegung, endet mit kleiner Bewegung nach unten
+            # print(str(self.activity) + "   " + str(self.z_pos))
+            if self.receiveActivity():
+                if self.z_pos < self.z_pos_old - self.t2:
+                    self.state = 2
+                    print("back to normal")
+            else:
+                self.state = 0
+        elif self.state == 4:  # schnelle Runterbewegung, endet mit kleiner Bewegung nach oben
+            if self.receiveActivity():
+                if self.z_pos > self.z_pos_old + self.t2:
+                    self.state = 2
+                    print("back to normal")
+            else:
+                self.state = 0
+        else:
+            self.state = 0
