74c2a18ebeb36881c988c722d0ea59956580c963
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2019-03-17 14:30:54 +0100

    SpotifyAPI

--- Swype/PC/main/serialHandling.py
+++ Swype/PC/main/serialHandling.py
@@ -2,6 +2,8 @@ import logic
 import time
 import spotifyAPI
 last = time.time()
+counter = 0
+progress = 0
 lastname = None
 logic = logic.Logic()
 logic.start(logic.getCOM())
@@ -11,10 +13,14 @@ while True:
     if time.time() - last > 1:
         #one second passed
         last = time.time()
-        spotifyAPI.getJson()
-        song, artist, progress = spotifyAPI.getInfo()
-        if song != lastname:
-            logic.sendAll(song,artist,progress)
-        else:
-            logic.sendTime(progress)
+        logic.sendTime(progress+counter)
+        counter += 1
+        if counter >= 5:
+            spotifyAPI.getJson()
+            song, artist, progress = spotifyAPI.getInfo()
+            counter = 0
+            if song != lastname:
+                logic.sendAll(song,artist,progress)
+            else:
+                logic.sendTime(progress)
     time.sleep(0.0001)
--- Swype/PC/main/test.py
+++ Swype/PC/main/test.py
@@ -1,8 +1,34 @@
+import win32api
 import spotifyAPI
 import time
+#Windows VirtualKeyCoed
+mediaNext = 0xB0
+mediaLast = 0xB1
+mediaPause = 0xB3
+
+def hwcode(Media):
+	hwcode = win32api.MapVirtualKey(Media, 0)
+	return hwcode
+
+def nextSong():
+    win32api.keybd_event(mediaNext,hwcode(mediaNext))
+
+def lastSong():
+    win32api.keybd_event(mediaLast,hwcode(mediaLast))
+
+def pause():
+    win32api.keybd_event(mediaPause,hwcode(mediaPause))
+
+def play():
+    win32api.keybd_event(mediaPause,hwcode(mediaPause))
 
 spotifyAPI.getToken()
-while 1:
-    spotifyAPI.getJson()
-    print(spotifyAPI.getInfo())
-    time.sleep(1)
+
+
+'''play()
+time.sleep(3)
+play()
+time.sleep(3)
+nextSong()
+time.sleep(3)
+lastSong()'''
