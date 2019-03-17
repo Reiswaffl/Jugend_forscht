bf540f74931781fd25c8a5ca7eaf416ad5124cfe
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2019-03-17 17:40:39 +0100

    Spotify APi

--- Swype/PC/main/serialHandling.py
+++ Swype/PC/main/serialHandling.py
@@ -1,26 +1,10 @@
 import logic
 import time
 import spotifyAPI
-last = time.time()
-counter = 0
-progress = 0
-lastname = None
+
 logic = logic.Logic()
 logic.start(logic.getCOM())
-spotifyAPI.getToken()
 while True:
     logic.handleInput()
-    if time.time() - last > 1:
-        #one second passed
-        last = time.time()
-        logic.sendTime(progress+counter)
-        counter += 1
-        if counter >= 5:
-            spotifyAPI.getJson()
-            song, artist, progress = spotifyAPI.getInfo()
-            counter = 0
-            if song != lastname:
-                logic.sendAll(song,artist,progress)
-            else:
-                logic.sendTime(progress)
+    #logic.spotifyHandling()
     time.sleep(0.0001)
