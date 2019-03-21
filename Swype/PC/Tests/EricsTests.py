85547735a144988885a02931d8f0bd834ca72249
Reiswaffl <38401280+Reiswaffl@users.noreply.github.com>
2019-03-21 18:27:40 +0100

    APITests

--- Swype/PC/Tests/EricsTests.py
+++ Swype/PC/Tests/EricsTests.py
@@ -1,8 +1,13 @@
-from spotify import spotifyAPI
+import serial
 import time
-spotifyAPI.getToken()
+com = "COM8"
+Pi_Serial = serial.Serial(com, 115200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+time.sleep(1)
+
+if Pi_Serial.isOpen():  # opens it again, if its open already (to minimize error rate)
+    Pi_Serial.close()
+    Pi_Serial.open()
 
 while 1:
-    spotifyAPI.getJson()
-    spotifyAPI.getInfo()
-    time.sleep(2)
+    Pi_Serial.write("Hello World")
+    time.sleep(5);
